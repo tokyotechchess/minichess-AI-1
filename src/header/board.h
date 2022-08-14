398e20f8522327fe5808934ce259ae6d518ad501
grkon <yo.for.user@gmail.com>
2022-08-14 18:17:58 +0900

    complete issue #8

--- src/header/board.h
+++ src/header/board.h
@@ -6,6 +6,7 @@ board.h
 Class "Board" and its method are defined.
 */
 
+#include <string>
 #include "util/error.h"
 #include "piece.h"
 
@@ -31,17 +32,44 @@ namespace minichess_AI
                 *110: Bishop
         */
         int files[5];
+        /*
+            Variable "turn" is the color of now turn.
+        */
+        Color turn;
+        /*
+            Variable "enpassantAblePawnFile" is the file of the pawn which can be a enpassant target.
+            When there is no such the target, the value is FILEERR.
+
+            example:
+            When turn == cWhite, and if black plays c5 to c3 on the pre-turn, enpassantAblePawnFile = CFILE
+        */
+        File enpassantAblePawnFile;
+        /*
+            Variable "castlingPossibility" stores possibilities of castling.
+
+
+            Color who  |
+            can castle | Value
+            -----------|----------
+            No one     | 0
+            White      | 1
+            Black      | 2
+            Both       | 3
+        */
+        int castlingPossibility;
 
     public:
         // Constructor
 
-        Board() { InitBoard(); }
+        Board() : turn(cWhite), enpassantAblePawnFile(FILEERR), castlingPossibility(3) { InitBoard(); }
 
         // methods
 
         MCError InitBoard();
         int *GetBoard();
         int GetSquare(File, Rank);
+        MCError SetSquare(File, Rank, Piece);
+        MCError SetBoardFEN(std::string fen);
         MCError Move(File, Rank, File, Rank);
     };
 
@@ -82,6 +110,170 @@ namespace minichess_AI
         int c = this->files[file];
         int r = ConvRankToWeight(rank);
         return (c & (0b1111 * r)) / r;
+    }
+
+    // set a piece of square
+    // if piece = EMPTYSQ, delete piece in the square, else, put a piece to the square
+    MCError Board::SetSquare(File file, Rank rank, Piece piece)
+    {
+        files[file] *= 0b111111111111111111111111 - ConvRankToWeight(rank) * 0b1111;
+        files[file] += ConvRankToWeight(rank) * (int)piece;
+
+        return mcet::NoErr;
+    }
+
+    // set pieces in the whole board by FEN
+    MCError Board::SetBoardFEN(std::string FEN)
+    {
+        File nowf = AFILE;
+        Rank nowr = RANK6;
+        char c;
+        int temp;
+        Piece temppiece;
+        int phaze = 0;
+        bool csetted = false;
+        bool finish = false;
+
+        // set all squares no piece
+        for (int i = AFILE; i <= EFILE; i++)
+        {
+            files[i] = 0;
+        }
+
+        for (int i = 0; i < FEN.length(); i++)
+        {
+            c = FEN[i];
+
+            if (finish)
+            {
+                break;
+            }
+
+            // to next phaze
+            if (c == ' ')
+            {
+                // finish reading FEN
+                if (phaze == 3)
+                {
+                    break;
+                }
+
+                phaze++;
+                continue;
+            }
+
+            // process at each phaze
+            switch (phaze)
+            {
+            case 0:
+                // piece placement
+
+                // empty squares
+                if ('0' <= c && c <= '9')
+                {
+                    temp = (int)nowf + int(c - '0');
+                    if (temp > (int)EFILE + 1)
+                    {
+                        return mcet::genFENErr(
+                            "RANK" + std::to_string((int)nowr + 1) + " has more than 6 files due to too many empty squares");
+                    }
+
+                    nowf = (temp == (int)EFILE + 1) ? FILEERR : (File)temp;
+                    break;
+                }
+
+                // pieces
+                temppiece = ConvFENCharToPiece(c);
+                if (temppiece != EMPTYSQ)
+                {
+                    if (nowf == FILEERR)
+                    {
+                        return mcet::genFENErr(
+                            "RANK" + std::to_string((int)nowr + 1) + " has more than 6 files due to too many pieces");
+                    }
+
+                    SetSquare(nowf, nowr, temppiece);
+
+                    nowf = (nowf == EFILE) ? FILEERR : File((int)nowf + 1);
+                    break;
+                }
+
+                // new rank
+                if (c == '/')
+                {
+                    // ignore not enough pieces error
+
+                    if (nowr == RANK1)
+                    {
+                        return mcet::genFENErr("This FEN has more than 7 ranks");
+                    }
+
+                    nowf = AFILE;
+                    nowr--;
+                    break;
+                }
+
+                return mcet::genFENErr("Wrong FEN expression about the piece placement");
+
+            case 1:
+                // turn
+
+                if (!csetted)
+                {
+                    if (c == 'w' || c == 'W')
+                    {
+                        turn = cWhite;
+                        csetted = true;
+                        break;
+                    }
+                    else if (c == 'b' || c == 'B')
+                    {
+                        turn = cBlack;
+                        csetted = true;
+                        break;
+                    }
+                }
+
+                return mcet::genFENErr("Wrong FEN expression about the color setting");
+
+            case 2:
+                // castling possibility
+                if (c == 'K')
+                {
+                    castlingPossibility += 1;
+                    break;
+                }
+                else if (c == 'k')
+                {
+                    castlingPossibility += 2;
+                    break;
+                }
+
+                return mcet::genFENErr("Wrong FEN expression about the castling possibility");
+
+            case 3:
+                // enpassant target
+                if (c == '-')
+                {
+                    enpassantAblePawnFile = FILEERR;
+                    finish = true;
+                }
+                else if ('a' <= c && c <= 'e')
+                {
+                    enpassantAblePawnFile = (File)(int)(c - 'a');
+                    finish = true;
+                }
+                else if ('A' <= c && c <= 'E')
+                {
+                    enpassantAblePawnFile = (File)(int)(c - 'A');
+                    finish = true;
+                }
+
+                return mcet::genFENErr("Wrong FEN expression about the en passant");
+            }
+
+            return mcet::NoErr;
+        }
     }
 
     // move piece
