52066a930a5299f4219b704499be92cf4e8b4086
Ryomatech <95959694+Ryomatech@users.noreply.github.com>
2022-09-08 00:52:13 +0900

    revised

--- src/header/commandline.h
+++ src/header/commandline.h
@@ -4,5 +4,14 @@
 
 namespace minichess_AI
 {
-
+    class CommandLine
+    {
+    private:
+        struct MCCSetting
+        {
+            bool displayCastlingPossibility;
+            bool displaySpendTurn;
+        };
+        MCCSetting setting;
+    };
 }
