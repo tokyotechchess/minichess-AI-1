bea6a5f45c5aa0009461f7bee5484a697515d742
Ryomatech <95959694+Ryomatech@users.noreply.github.com>
2022-09-09 04:32:31 +0900

    良くわからん教えて

--- src/header/commandline.cpp
+++ src/header/commandline.cpp
@@ -1,1 +1,15 @@
 #include "commandline.h"
+
+namespace minichess_AI
+{
+    MCError CommandLine::DisplayBoard(Board *b)
+    {
+        std::cout << '--|-----------';
+        std::cout << '  | a b c d e';
+        std::cout << '--|-----------';
+        for (Rank r = RANK6; r >= RANK1; r--)
+        {
+            std::cout << (*b).GetSquare(Square{Square{AFILE, RANK1}});
+        }
+    }
+}
