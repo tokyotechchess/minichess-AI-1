1f07d539533777d8ceba4e17713160159fcff420
Morikawa Tenma <tenten0426motoyosi@gmail.com>
2022-10-28 14:04:01 +0900

    cout<<again2

--- src/header/piece.h
+++ src/header/piece.h
@@ -107,7 +107,7 @@ namespace minichess_AI
     Piece ConvFENCharToPiece(char c);
 
     char ConvPieceToFENChar(Piece p);
-    char ConvSquareToPGNSString(Square sq);
+    char ConvSquareToPGNString(Square sq);
     inline std::ostream &operator<<(std::ostream &os, const Piece &p)
     {
         os << ConvPieceToFENChar(p);
