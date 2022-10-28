630f4921ca1f8001645494580af8d7f4f09d1630
grkon <yo.for.user@gmail.com>
2022-10-29 08:55:31 +0900

    とりあえず形に...

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,50 @@ about FEN
 */
 
 #include "collection.h"
+#include <string>
 
 using namespace minichess_AI;
+using std::string;
 
 int main()
 {
+  Board b, copy;
+  minichessAI AI;
+  CommandLine cmd;
+  string in;
+  Square bf, bt, mvf, mvt;
+  Piece bp, pr;
+  MCError e;
+
+  cmd.ChangeSetting(MCCSetting{true, true, true});
+
+  while (!b.IsMated())
+  {
+    copy = b;
+    cmd.DisplayBoard(&b);
+    std::cout << "evaluation : " << AI.evaluate(b) << std::endl;
+    std::cout << "user > ";
+    getline(std::cin, in);
+
+    if (in == "mb")
+    {
+      b = copy;
+    }
+    else if (in == "bm")
+    {
+      AI.GetBestMove(&bf, &bt, &bp);
+      b.MoveForce(bf, bt, bp);
+    }
+    else
+    {
+      mvf = Square{File(in[0] - 'a'), Rank(in[1] - '1')};
+      mvt = Square{File(in[2] - 'a'), Rank(in[3] - '1')};
+      pr = ConvFENCharToPiece(in[4]);
+      e = b.Move(mvf, mvt, pr);
+      if (e != mcet::NoErr)
+      {
+        std::cout << e.DisplayError() << std::endl;
+      }
+    }
+  }
 }
